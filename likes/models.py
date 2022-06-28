eff59e257b98e4a0260a8fac116ffbbd41311726
Monika Jakobsson <nimrin81@gmail.com>
2022-06-28 23:21:01 +0000

    add list model and views

--- likes/models.py
+++ likes/models.py
@@ -10,12 +10,14 @@ class Like(models.Model):
     'unique_together' makes sure a user can't like the same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
-        unique_together = [['owner', 'post']]
+        unique_together = ['owner', 'post']
 
     def __str__(self):
         return f'{self.owner} {self.post}'
